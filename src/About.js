24b1527c3ea907e88940d122c86d619d45236df1
vashanti <vashanti@gmail.com>
2022-07-23 12:05:13 -0400

    Initial

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return ( 
+        <div>
+            <h1>About Me</h1>
+            <p>I am an alien from a different universe</p>
+        </div>
+     );
+}
+
+export default About;
